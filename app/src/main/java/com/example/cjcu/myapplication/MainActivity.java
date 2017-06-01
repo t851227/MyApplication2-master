a03759858f548c5a84692d91984f911380106bb3
t851227 <at851227@gmail.com>
2017-06-01 14:17:36 +0800

    第二次修改

--- app/src/main/java/com/example/cjcu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cjcu/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView list = (ListView) findViewById(R.id.list);
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
-        list.setAdapter(adapter);
+        //final ListView list = (ListView) findViewById(R.id.list);
+        final Spinner spinner = (Spinner)findViewById(R.id.spinner);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruit);
+        ArrayAdapter adapter1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,fruit);
+
+        //list.setAdapter(adapter);
+        spinner.setAdapter(adapter1);
+
+        //list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //    @Override
+        //    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //        Toast.makeText(MainActivity.this,list.getItemAtPosition(position).toString() , Toast.LENGTH_SHORT).show();
+        //    }
+        //});
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,list.getItemAtPosition(position).toString() , Toast.LENGTH_SHORT).show();
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,spinner.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
